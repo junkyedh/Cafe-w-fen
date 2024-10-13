a6ef987933ea1f5c6a6eccfae3804759ffe46ac9
sangt <thachs@hqsoft.com.vn>
2024-10-13 16:01:54 +0700

    finish ui edit product

--- src/components/product-list/tab/tabLayout/ProductListLayout.tsx
+++ src/components/product-list/tab/tabLayout/ProductListLayout.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import TabContent from "../tabContent/content/TabContent";
+import TabEditProduct from "../tabEdit/tabEditProduct";
+import "./ProductListLayout.scss"
 
 
 const ProductListLayout = () => {
+
+  const [isEditing, setIsEditing] = useState(false);
+
     return(
-        <div className="partner-list-layout">
-        <p className="title-text">Product List</p>
-        <div className="tab-layout-div">
-          <TabContent />
+
+        <div className="container-1">
+          <div className="partner-list-layout">
+              <p className="title-text">Product List</p>
+              <div className="tab-layout-div">
+              <TabContent />
+              </div>          
+          </div>
+
+            <TabEditProduct/>
         </div>
-      </div>
+      
+       
+
+       
     )
 };
 

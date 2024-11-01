9e8cd2089583b103b3d3d5d405fbf688889dde23
BlackSecret164 <tienanhchilang12@gmail.com>
2024-11-02 00:08:48 +0700

    first commit

---
+++ BackEnd/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const { Pool } = require("pg");
+
+app.use(cors());
+app.use(express.json());
+
+const {PGHOST, PGDATABASE, PGUSER, PGPASSWORD} = process.env;
+
+const pool = new Pool({
+    host: PGHOST,
+    database: PGDATABASE,
+    user: PGUSER,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl:{
+        require: true,
+        rejectUnauthorized: false
+    }
+})
+
+app.get("/", async (req, res) =>{
+    const client = await pool.connect();
+
+    try {
+        const result = await client.query("SELECT * FROM staff");
+
+        res.json(result.rows);
+    } catch (errors) {
+        console.log(errors)
+    } finally{
+        client.release();
+    }
+
+    res.status(404);
+})
+
+app.listen(3000, console.log("Server Running"));
